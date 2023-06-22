49cbab4d3e8ca8e35d807d5d8a27aa207b2a6eca
chevogomez-git <chevogomez-git@users.noreply.github.com>
2023-06-21 20:53:16 -0600

    Added files register.html, login.html, changes to css and app.js

--- app.js
+++ app.js
@@ -7,11 +7,21 @@ const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000");
-    console.log("Folder Path = " + publicPath);
-});
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en puerto 3000");
+    console.log("Folder Path = " + publicPath);
 });
